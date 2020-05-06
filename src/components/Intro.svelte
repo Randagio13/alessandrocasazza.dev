a6b0c67698af6344cd8b6d18899f87b239d4773c
Alessandro Casazza <imasterale@gmail.com>
2020-05-06 19:38:11 +0200

    Corrected spelling

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -6,6 +6,6 @@
   <p>I'm Alessandro!</p>
   <p>Senior Frontend Developer at <a target="_blank" rel="noreferrer" href="//commercelayer.io" aria-label="Commerce Layer">Commerce Layer</a>,</p>
   <p>Javascript Lover 💛, <a target="_blank" rel="noreferrer" href="//reactjs.org" aria-label="ReactJS">ReactJS</a> expert &</p>
-  <p><a target="_blank" rel="noreferrer" href="//jamstack.org" aria-label="JAMStack">JAMStack</a> advocate.</p>
+  <p><a target="_blank" rel="noreferrer" href="//jamstack.org" aria-label="JAMStack">Jamstack</a> advocate.</p>
   <a href="mailto:alessandro.casazza@icloud.com" class="no-underline inline-block bg-gray-800 px-5 py-3 text-gray-300 rounded mt-4 shadow">Contact me</a>
 </div>
